c89305dd3ae34adebef53e0f81c39f6a34531892
dmolio <dmolio@molnafsystems.com>
2024-09-30 21:11:09 -0400

    modified cicd.yaml

---
+++ tests/test_visitor_counter.py
@@ -0,0 +1,34 @@
+import json
+import pytest
+from moto import mock_dynamodb
+import boto3
+from lambda.visitor_counter import lambda_handler
+
+@mock_dynamodb
+def test_lambda_handler():
+    # Create mock DynamoDB table
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.create_table(
+        TableName='resume-challenge',
+        KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
+        AttributeDefinitions=[{'AttributeName': 'id', 'AttributeType': 'S'}],
+        BillingMode='PAY_PER_REQUEST'
+    )
+
+    # Initialize the counter
+    table.put_item(Item={'id': '0', 'views': 0})
+
+    # Test the lambda function
+    event = {}
+    context = {}
+    response = lambda_handler(event, context)
+
+    assert response['statusCode'] == 200
+    body = json.loads(response['body'])
+    assert 'views' in body
+    assert body['views'] == 1
+
+    # Test it again to make sure the counter increments
+    response = lambda_handler(event, context)
+    body = json.loads(response['body'])
+    assert body['views'] == 2
